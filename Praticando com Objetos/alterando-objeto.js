b6e7052fdbe3ff690084d2bad3e58f39293acd0e
Rayan Correa <111525526+raycorrea@users.noreply.github.com>
2024-09-24 23:15:18 -0300

    Add files via upload

---
+++ Praticando com Objetos/alterando-objeto.js
@@ -0,0 +1,17 @@
+const estudante = {
+  nome: 'José Silva',
+  idade: 32,
+  cpf: '12312312312',
+  turma: 'JavaScript'
+}
+
+estudante.telefone = '551199999999';
+console.log(estudante.telefone);
+console.log(estudante);
+
+estudante.nome = 'José Souza';
+console.log(estudante);
+
+const estudante2 = {};
+estudante2.nome = 'Ana Maria'
+console.log(estudante2);
---
+++ Praticando com Objetos/dados-valores.js
@@ -0,0 +1,10 @@
+const estudante = {
+  nome: 'José Silva',
+  idade: 32,
+  cpf: '12312312312',
+  turma: 'JavaScript',
+  bolsista: true,
+  telefones: ['551199999998', '551199999993'],
+}
+
+console.log(estudante.telefones[0]);
